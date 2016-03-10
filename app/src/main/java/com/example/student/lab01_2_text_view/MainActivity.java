3c34705ea621b794ddd18daf2ce9e05513963123
smile800606 <smile800606@yahoo.com.tw>
2016-03-10 11:23:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/lab01_2_text_view/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.student.lab01_2_text_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab01_4_image_view);
+    }
+}
